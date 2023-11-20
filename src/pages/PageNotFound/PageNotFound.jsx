4b860890f32abe6c8ccb3313e7a4e937d464cd33
Lise <lise692@gmail.com>
2023-11-20 20:59:47 +0100

    adding some style and illustration to 404 page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,13 @@
 import s from "./style.module.css";
 
 export function PageNotFound(props) {
-  return <>404 - Page not found</>;
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h2>404 - Page not found</h2>
+      <img
+        src={require("../../assets/images/404_error.png")}
+        alt="404 illustration"
+      />
+    </div>
+  );
 }
