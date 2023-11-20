96f49198f249ad188a695f00db63d71eb1eea675
Lise <lise692@gmail.com>
2023-11-20 20:32:00 +0100

    Adding searching fonctionnality

--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -5,8 +5,7 @@ import s from "./style.module.css";
 import { NoteAPI } from "api/note-api";
 import { deleteNote } from "store/note/note-slice";
 
-export function NoteList(props) {
-  const noteList = useSelector((store) => store.NOTE.noteList);
+export function NoteList({ noteList }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
