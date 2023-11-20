11dc55e73822eb73ae9c97556e708de0696aec20
Lise <lise692@gmail.com>
2023-11-20 20:14:07 +0100

    adding searchbar component + styling

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,9 +1,17 @@
+import { SearchBar } from "components/SearchBar/SearchBar";
 import s from "./style.module.css";
 import { NoteList } from "containers/NoteList/NoteList";
+import { useState } from "react";
 
 export function NoteBrowse(props) {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <>
+      <SearchBar
+        placeholder="Search your ticket"
+        onTextChange={setSearchText}
+      />
       <NoteList />
     </>
   );
